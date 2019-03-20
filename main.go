88102ed30b153e5eed144a97e6c4c3744d52dae7
tyrannogina <sara.latorre@pod-point.com>
2019-03-20 22:53:36 +0000

    Simplified version of the list with some styling

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"time"
 
 	firebase "firebase.google.com/go"
 	"google.golang.org/appengine" // Required external App Engine library
@@ -22,30 +21,16 @@ var (
 	indexTemplate = template.Must(template.ParseFiles("index.html"))
 )
 
-type templateParams struct {
-	Notice        string
-	ItemName      string
-	ShoppingLists []ShoppingList
-}
-
-// User : owner of the shopping list, has a name and a UserID
-type User struct {
-	Name   string
+// ShoppingItem : Item of the shopping list. Contains a name.
+type ShoppingItem struct {
 	UserID string
+	Name   string
 }
 
-// Item : Item of the shopping list. Contains a name and can be striked through.
-type Item struct {
-	Name    string
-	Striked bool
-}
-
-// ShoppingList : allows to save several items. Has an author with their corresponding id, creation time, name and list of items.
-type ShoppingList struct {
-	//Owner     User,
-	Name      string
-	Items     []Item
-	CreatedAt time.Time
+type templateParams struct {
+	Notice   string
+	ItemName string
+	Items    []ShoppingItem
 }
 
 func main() {
@@ -67,29 +52,19 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	params := templateParams{}
 
-	q := datastore.NewQuery("ShoppingList").Order("-CreatedAt").Limit(20)
+	q := datastore.NewQuery("ShoppingItem").Limit(20)
 
 	var keys []*datastore.Key
 	var err error
 
-	if keys, err = q.GetAll(ctx, &params.ShoppingLists); err != nil {
-		log.Errorf(ctx, "Getting shopping lists: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		params.Notice = "Couldn't get latest shopping Lists. Refresh?"
-		indexTemplate.Execute(w, params)
+	if keys, err = q.GetAll(ctx, &params.Items); err != nil {
+		handleError(ctx, err, "datastore.GetAll: %v", "Couldn't get latest items. Refresh? %v!", params, w)
 		return
-	}
-
-	for i := 0; i < len(keys); i++ {
-		params.Notice += fmt.Sprintf("Keys: %v", keys)
 	}
 
 	if false {
 		if err := datastore.DeleteMulti(ctx, keys); err != nil {
-			log.Errorf(ctx, "Shopping lists could not be deleted: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			params.Notice = "Shopping lists could not be deleted. Refresh?"
-			indexTemplate.Execute(w, params)
+			handleError(ctx, err, "datastore.DeleteMulti: %v", "Items could not be deleted. Refresh? %v!", params, w)
 			return
 		}
 	}
@@ -103,90 +78,55 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	if (itemName) == "" {
 		w.WriteHeader(http.StatusBadRequest)
-		params.Notice = "No message provided"
+		params.Notice = "Write a new item for the list before you submit"
 		indexTemplate.Execute(w, params)
 		return
 	}
-	/*
-		// Create a new Firebase App.
-		app, err := firebase.NewApp(ctx, firebaseConfig)
-		if err != nil {
-			params.Notice = "Couldn't authenticate 1. Try logging in again?"
-			log.Errorf(ctx, "firebase.NewApp: %v", err)
-			params.ItemName = itemName // Preserve their input so they can try again.
-			indexTemplate.Execute(w, params)
-			return
-		}
-		// Create a new authenticator for the app.
-		authClient, err := app.Auth(ctx)
-		if err != nil {
-			params.Notice = "Couldn't authenticate 2. Try logging in again?"
-			log.Errorf(ctx, "app.Auth: %v", err)
-			params.ItemName = itemName // Preserve their input so they can try again.
-			indexTemplate.Execute(w, params)
-			return
-		}
 
-		// Verify the token passed in by the user is valid.
-		tok, err := authClient.VerifyIDTokenAndCheckRevoked(ctx, r.FormValue("token"))
-		if err != nil {
-			params.Notice = fmt.Sprintf("Couldn't authenticate 3. Try logging in again? auth.VerifyIDAndCheckRevoked: %v", err)
-			log.Errorf(ctx, "auth.VerifyIDAndCheckRevoked: %v", err)
-			params.ItemName = itemName // Preserve their input so they can try again.
-			indexTemplate.Execute(w, params)
-			return
-		}
-		// Use the validated token to get the user's information.
-		user, err := authClient.GetUser(ctx, tok.UID)
-		if err != nil {
-			params.ItemName = itemName // Preserve their input so they can try again.
-			handleError(ctx, err, "auth.GetUser: %v", ""Couldn't authenticate 4. Try logging in again? %v!", params, w)
-			return
-		}
-	*/
-	newItem := Item{
-		Name:    itemName,
-		Striked: false,
+	// Create a new Firebase App.
+	app, err := firebase.NewApp(ctx, firebaseConfig)
+	if err != nil {
+		params.ItemName = itemName // Preserve their input so they can try again.
+		handleError(ctx, err, "firebase.NewApp: %v", "Couldn't authenticate 1. Try logging in again? %v!", params, w)
+		return
 	}
-	// Insert new shopping list if one does not exist already
-	if len(params.ShoppingLists) == 0 {
-		name := "My groceries"
-
-		shoppingList := ShoppingList{
-			Name:      name,
-			Items:     []Item{newItem},
-			CreatedAt: time.Now(),
-		}
-
-		key := datastore.NewIncompleteKey(ctx, "ShoppingList", nil)
-
-		if _, err := datastore.Put(ctx, key, &shoppingList); err != nil {
-			handleError(ctx, err, "datastore.Put: %v", "Couldn't add new shopping list list. Try again? %v!", params, w)
-			return
-		}
-
-		params.ShoppingLists = append([]ShoppingList{shoppingList}, params.ShoppingLists...)
-	} else {
-		// Attach item to existing shopping list
-		key := datastore.NewIncompleteKey(ctx, "Item", keys[0])
-		params.Notice += fmt.Sprintf("OMG whats the new item key %v!", key)
-		var currentShoppingList ShoppingList
-
-		if err := datastore.Get(ctx, keys[0], &currentShoppingList); err != nil {
-			handleError(ctx, err, "datastore.Put: %v", "Couldn't add new item to the list. Try again? %v!", params, w)
-			return
-		}
-
-		if _, err := datastore.Put(ctx, key, &newItem); err != nil {
-			handleError(ctx, err, "datastore.Put: %v", "Couldn't add new item to the list. Try again? %v!", params, w)
-			return
-		}
-
-		params.Notice += fmt.Sprintf("OMG whats the new item %v!", newItem)
-
+	// Create a new authenticator for the app.
+	authClient, err := app.Auth(ctx)
+	if err != nil {
+		params.ItemName = itemName // Preserve their input so they can try again.
+		handleError(ctx, err, "app.Auth: %v", "Couldn't authenticate 2. Try logging in again? %v!", params, w)
+		return
+	}
+	// Verify the token passed in by the user is valid.
+	tok, err := authClient.VerifyIDTokenAndCheckRevoked(ctx, r.FormValue("token"))
+	if err != nil {
+		params.ItemName = itemName // Preserve their input so they can try again.
+		handleError(ctx, err, "authClient.VerifyIDAndCheckRevoked: %v", "Couldn't authenticate 3. Try logging in again? %v!", params, w)
+		return
+	}
+	// Use the validated token to get the user's information.
+	user, err := authClient.GetUser(ctx, tok.UID)
+	if err != nil {
+		params.ItemName = itemName // Preserve their input so they can try again.
+		handleError(ctx, err, "auth.GetUser: %v", "Couldn't authenticate 4. Try logging in again? %v!", params, w)
+		return
 	}
 
-	params.Notice += fmt.Sprintf("Hey You! %v\n", params.ShoppingLists) //user.DisplayName)
+	newItem := ShoppingItem{
+		UserID: user.UID,
+		Name:   itemName,
+	}
+
+	// Attach item to existing shopping list
+	key := datastore.NewIncompleteKey(ctx, "ShoppingItem", nil)
+
+	if _, err := datastore.Put(ctx, key, &newItem); err != nil {
+		handleError(ctx, err, "datastore.Put: %v", "Couldn't add new item to the list. Try again? %v!", params, w)
+		return
+	}
+
+	params.Items = append([]ShoppingItem{newItem}, params.Items...)
+
 	indexTemplate.Execute(w, params)
 }
 
